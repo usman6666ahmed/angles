4c832bc967c5d78d4855734b096801d630587a4e
usman6666ahmed <usman.imtiaz@devsinc.com>
2022-09-15 19:48:42 +0500

    New component, Created a todo list
    * Added navbar component
    * Created a full todo list in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+interface Todo {
+  title: string;
+  done: boolean;
+  id: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angles';
+  newTodoText=  ""
+  todos: Todo[] = [
+    { title: 'Learn Angular', done: false, id: 1 },
+    { title: 'Learn TypeScript', done: false, id: 2 },
+    { title: 'Learn ASP.NET', done: false, id: 3 },
+  ];
+
+  alertMe() {
+    alert('You have been alerted!');
+  }
+
+  onChange(e:any){
+    this.newTodoText = e.target.value
+    console.log(this.newTodoText)
+  }
+
+  completeTodo(id: number) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
+  }
+
+  newTodo() {
+    this.todos.push({
+      title: this.newTodoText,
+      id: this.todos.length + 1,
+      done: false,
+    });
+    this.newTodoText = "";
+  }
 }
